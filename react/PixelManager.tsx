236c89e033ed306bc0dfeb2f6ccef886f19e46c9
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-12-20 17:04:32 -0300

    Fix typescript typings

--- react/PixelManager.tsx
+++ react/PixelManager.tsx
@@ -1,25 +1,25 @@
 import React, { Fragment } from 'react'
-import { DataProps, graphql } from 'react-apollo'
+import { ChildDataProps, DataProps, graphql } from 'react-apollo'
 
 import PixelIFrame from './PixelIFrame'
 import installedPixelsQuery from './queries/installedPixelsQuery.gql'
 
-interface Response {
+interface Data {
   installedPixels: string[]
 }
 
-type Props = DataProps<Response>
+const withInstalledPixels = graphql<{}, Data>(installedPixelsQuery)
 
-const PixelManager: React.SFC<Props> = ({ data: { loading, installedPixels } }) => {
-  if (loading || !installedPixels) {
+const PixelManager = withInstalledPixels(({ data }) => {
+  if (!data || data.loading || !data.installedPixels) {
     return null
   }
 
   return (
     <Fragment>
-      {installedPixels.map(pixel => <PixelIFrame key={pixel} pixel={pixel} />)}
+      {data.installedPixels.map(pixel => <PixelIFrame key={pixel} pixel={pixel} />)}
     </Fragment>
   )
-}
+})
 
-export default graphql<Props>(installedPixelsQuery)(PixelManager)
+export default PixelManager
