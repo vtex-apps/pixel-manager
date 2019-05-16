e197ea2ef9289ddf65cd0f697337750eeb993592
Breno Calazans <brenocalazans@gmail.com>
2019-05-16 18:17:25 -0300

    Add postMessage event on load

--- node/templates.ts
+++ node/templates.ts
@@ -1,6 +1,7 @@
 import { map } from 'ramda'
 
 interface TemplateInput {
+  appId: string
   scripts?: string[]
   settings: {
     [varName: string]: any
@@ -9,14 +10,22 @@ interface TemplateInput {
 
 const scriptSrcToTag = (src: string) => `<script src="${src}" defer></script>`
 
-export const html = ({ settings = {}, scripts = [] }: TemplateInput) => `<!DOCTYPE html>
+export const html = ({ appId, settings = {}, scripts = [] }: TemplateInput) => `<!DOCTYPE html>
 <html>
 <head>
-  ${map(scriptSrcToTag, scripts).join('\n')}
+  ${map(scriptSrcToTag, scripts).join('\n  ')}
   <script>
     window.__SETTINGS__ = ${JSON.stringify(settings)};
   </script>
 </head>
 <body>
+
+<script>
+  (function() {
+    window.addEventListener('load', function() {
+      window.parent.postMessage('pixel:ready:${appId}', '*');
+    })
+  })()
+</script>
 </body>
 </html>`
