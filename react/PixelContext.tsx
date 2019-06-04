55eef2aff56b81493e34adc048bca7596643ba89
Lucas Bebber <lucasbbebber@gmail.com>
2019-06-04 15:42:11 -0300

    Fix comment wording

--- react/PixelContext.tsx
+++ react/PixelContext.tsx
@@ -146,8 +146,8 @@ class PixelProvider extends PureComponent<Props> {
       window.postMessage(eventData, window.origin)
     } catch (e) {
       // IE and Edge have a bug on postMessage inside promises.
-      // Ignoring for now, will try to find a fix that makes
-      // postMessage work
+      // Ignoring for now, will try to find a workaround that
+      // makes postMessage work on those browsers.
       // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/14719328/
     }
   }
