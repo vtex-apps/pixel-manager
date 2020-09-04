333e7810239a46c87707f1a2a7132c129d797f8c
victorhmp <victor2142@gmail.com>
2020-09-04 15:52:18 -0300

    Simplify 'shouldCallEventHandler' logic

--- react/usePixelEventCallback.ts
+++ react/usePixelEventCallback.ts
@@ -9,28 +9,25 @@ export const shouldCallEventHandler = (
 ) => {
   const receivedEventId = Boolean(eventId)
   const receivedEventName = Boolean(eventName)
-  const eventHasId = Boolean(pixelEventData.id)
-  const eventHasName = Boolean(pixelEventData.event)
-
-  let result = false
+  const hasEventId = Boolean(pixelEventData.id)
+  const hasEventName = Boolean(pixelEventData.event)
 
   // These checks are to make sure that handlers are only called when
   // both the 'eventId' (if received) and the 'eventName' (if received)
   // match the event being processed.
   // If any of those arguments were NOT received, only the one that was
   // received should be taken into account.
-  if (receivedEventId && eventHasId && pixelEventData.id === eventId) {
-    result =
-      !receivedEventName || (eventHasName && pixelEventData.event === eventName)
-  } else if (
-    receivedEventName &&
-    eventHasName &&
-    pixelEventData.event === eventName
-  ) {
-    result = !receivedEventId || (eventHasId && pixelEventData.id === eventId)
+  if (receivedEventId && hasEventId && pixelEventData.id === eventId) {
+    return (
+      !receivedEventName || (hasEventName && pixelEventData.event === eventName)
+    )
   }
 
-  return result
+  if (receivedEventName && hasEventName && pixelEventData.event === eventName) {
+    return !receivedEventId || (hasEventId && pixelEventData.id === eventId)
+  }
+
+  return false
 }
 
 const usePixelEventCallback = ({
